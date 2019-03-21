87b1d82cebe8a8789872371f55c861fbfb9fbc9b
2nkyung <92daisychoi@gmail.com>
2019-03-21 17:49:20 +0900

    3/21 첫연동

---
+++ src/com/osf/test/db/DBCon.java
@@ -0,0 +1,72 @@
+package com.osf.test.db;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import com.osf.test.io.ReadFile;
+
+public class DBCon {
+	private static final String URL;
+	private static final String USER;
+	private static final String PASSWORD;
+	private static final String DRIVER;
+	private static Connection con;
+	static {
+		InputStream is = ReadFile.class.getResourceAsStream("/com/osf/test/config/db.properties");
+		Properties prop = new Properties();
+		try {
+			prop.load(is);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		URL = prop.getProperty("url");
+		USER = prop.getProperty("user");
+		PASSWORD = prop.getProperty("password");
+		DRIVER = prop.getProperty("classname");
+	}
+
+	public static Connection getCon() {
+		if(con==null) {
+			try {
+				Class.forName(DRIVER);
+				con = DriverManager.getConnection(URL, USER, PASSWORD);
+			} catch (ClassNotFoundException e) {
+			
+				e.printStackTrace();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return con;
+		
+	}
+
+	public static void close() {
+		if (con != null) {
+			try {
+				if (!con.isClosed()) {
+					con.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		con = null;
+	}
+
+	public static void main(String[] args) {
+		getCon();
+		close();
+		
+		System.out.println(URL);
+		System.out.println(USER);
+		System.out.println(PASSWORD);
+		System.out.println(DRIVER);
+	}
+
+}
